1404043343b580a83ae2447ba5e3be54c0443f6c
Jarian Martinez <jarianm2@nycstudents.net>
2025-02-10 14:31:38 +0000

    big boom

--- src/main/java/com/example/project/Delimiter/Delimiters.java
+++ src/main/java/com/example/project/Delimiter/Delimiters.java
@@ -18,15 +18,42 @@ public class Delimiters {
     /** Returns an ArrayList of delimiters from the array tokens, as described in part (a). */
     public ArrayList<String> getDelimitersList(String[] tokens) {
         /* to be implemented in part (a) */
-        return new ArrayList<String>();
+        ArrayList<String> delim = new ArrayList<String>();
+        for(int i = 0; i < tokens.length; i++) {
+            if(tokens[i].equals(openDel)) {
+                delim.add(tokens[i]);
+            }
+            if(tokens[i].equals(closeDel)) {
+                delim.add(tokens[i]);
+            }
+        }
+        return delim;
     }
     
 
     /** Returns true if the delimiters are balanced and false otherwise, as described in part (b).
      *  Precondition: delimiters contains only valid open and close delimiters.
      */
-    public boolean isBalanced(ArrayList<String> delimiters) {
+    public boolean isBalanced(String[] delimiters) {
         /* to be implemented in part (b) */
-        return false;
+        int count1 = 0;
+        int count2 = 0;
+        for(int i = 0; i < delimiters.length; i++) {
+            if(delimiters[i].equals(openDel)) {
+                count1++;
+            }
+            if(delimiters[i].equals(closeDel)) {
+                count2++;
+            }
+            if(count2 > count1) {
+                return false;
+            }
+        }
+        if(count1 != count2) {
+            return false;
+        } else {
+            return true;
+        }
     }
+
 }
