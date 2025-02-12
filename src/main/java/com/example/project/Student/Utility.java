0e709e068072ee57ff6e127aa49c62ba28519848
Jarian Martinez <jarianm2@nycstudents.net>
2025-02-12 14:29:51 +0000

    wow

--- src/main/java/com/example/project/Student/Utility.java
+++ src/main/java/com/example/project/Student/Utility.java
@@ -5,6 +5,12 @@ import java.util.Arrays;
 public class Utility {
 
     public static ArrayList<Student> sortStudents(ArrayList<Student> list){
+        ArrayList<Student> student = new ArrayList<Student>();
+        for(int i = 1; i <= list.size(); i++) {
+            if(list.get(i - 1).getLastName().compareTo(list.get(i).getLastName()) > 1) {
+                student.add(list.get(i - 1));
+            }
+        }
         return list;
     }
 
